a1ae23655ec5e8be4c17c487130232770bed8a6f
willcod <william.wow@icloud.com>
2022-03-20 22:07:48 +0800

    Refactor Speaker component

--- pages/index.js
+++ pages/index.js
@@ -17,52 +17,63 @@ const Sessions = (props) => {
   );
 };
 
+const SpeakerImage = ({ id, first, last }) => {
+  return (
+    <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
+      <img
+        className=" contain-fit"
+        src={`/images/speaker-${id}.jpg`}
+        width="300"
+        alt={`${first} ${last}`}
+      />
+    </div>
+  );
+};
+
+const SpeakerDemographics = ({
+  first,
+  last,
+  bio,
+  company,
+  twitterHandle,
+  favorite,
+}) => {
+  return (
+    <div className=" speaker-info">
+      <div className=" d-flex justify-content-between mb-3">
+        <h3 className=" text-truncate w-200">
+          {first} {last}
+        </h3>
+      </div>
+      <div>
+        <p>
+          {bio} {company} {twitterHandle} {favorite}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const Speaker = ({ speaker }) => {
+  const { id, first, last, sessions } = speaker;
+
+  return (
+    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
+      <div className="card card-height p-4 mt-4">
+        <SpeakerImage id={id} first={first} last={last} />
+        <SpeakerDemographics {...speaker} />
+        <Sessions sessions={sessions} />
+      </div>
+    </div>
+  );
+};
 
 const IndexPage = () => {
   return (
     <div className="container speakers-list">
       <div className="row">
         {data.map(function (speaker) {
-          const {
-            id,
-            bio,
-            first,
-            last,
-            favorite,
-            twitterHandle,
-            company,
-            sessions,
-          } = speaker;
-          return (
-            <div
-              key={id}
-              className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12"
-            >
-              <div className="card card-height p-4 mt-4">
-                <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
-                  <img
-                    className=" contain-fit"
-                    src={`/images/speaker-${id}.jpg`}
-                    width="300"
-                    alt={`${first} ${last}`}
-                  />
-                </div>
-                <div className=" speaker-info">
-                  <div className=" d-flex justify-content-between mb-3">
-                    <h3 className=" text-truncate w-200">
-                      {first} {last}
-                    </h3>
-                  </div>
-                  <div>
-                    <p>
-                      {bio} {company} {twitterHandle} {favorite}
-                    </p>
-                  </div>
-                </div>
-                <Sessions sessions={sessions} />
-              </div>
-            </div>
-          );
+          return <Speaker key={speaker.id} speaker={speaker} />;
         })}
       </div>
     </div>
